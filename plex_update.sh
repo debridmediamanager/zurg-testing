541472b97677073a628954c295e5fc51519d21b6
Ben Adrian Sarmiento <me@bensarmiento.com>
2024-01-10 02:07:03 +0100

    Update plex_update.sh

--- plex_update.sh
+++ plex_update.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# PLEX PARTIAL SCAN script or PLEX UPDATE script
+# When zurg detects changes, it can trigger this script IF your config.yml contains
+# on_library_update: sh plex_update.sh "$@"
+
 # this uses python3
 # docker compose exec zurg apk add python3
 # sudo apt install python3
 
 plex_url="http://yourplexip:32400" # If you're using zurg inside a Docker container, by default it is 172.17.0.1:32400
-token="yourplextoken"
-zurg_mount="/mnt/zurg" # replace with your zurg mount
+token="yourplextoken" # open Plex in a browser, open dev console and copy-paste this: window.localStorage.getItem("myPlexAccessToken")
+zurg_mount="/mnt/zurg" # replace with your zurg mount path, ensure this is what Plex sees
 
 # Get the list of section IDs
 section_ids=$(curl -sLX GET "$plex_url/library/sections" -H "X-Plex-Token: $token" | xmllint --xpath "//Directory/@key" - | sed 's/key="//g' | tr '"' '\n')
