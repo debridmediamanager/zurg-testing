9ef96cf03805d19a1ce2e5a9c3819c560b17b46a
fatbob01 <fatbob01@hotmail.com>
2024-02-14 11:55:52 -0500

    Update plex_update.ps1
    updated with latest script

--- plex_update.ps1
+++ plex_update.ps1
@@ -1,8 +1,16 @@
 Add-Type -AssemblyName System.Web
 
 # Plex server details - EDIT BELOW
-$plexUrl = "http://YourPlexIP:32400"
-$plexToken = "YourTokenHere"
+$plexUrl = "http://plex-ip:32400"
+$plexToken = "your-plex-token"
+
+# Replace with your mount - EDIT BELOW
+$mount = "Z:"
+
+$path = $args[2]
+
+# Set how many times you want the script to retry if the folder has not yet been added
+$retryAmount = 30
 
 # Function to URL encode a string
 function UrlEncode($value) {
@@ -10,63 +18,46 @@ function UrlEncode($value) {
 }
 
 # Example path to a log - EDIT BELOW
-Start-Transcript -Path "C:\Path\To\zurg-testing\plex_update.log"
+Start-Transcript -Path "C:\Zurg\zurg-testing\logs\plex_update.log"
 
 # Function to trigger library update for a specific folder
-function UpdateFolder($folder, $directory) {
+function UpdateFolder($retries) {
     $section_ids = (Invoke-WebRequest -Uri "$plexUrl/library/sections" -Headers @{"X-Plex-Token" = $plexToken} -UseBasicParsing -Method Get).Content |
-                   Select-Xml -XPath "//Directory/@key" |
-                   ForEach-Object { $_.Node.Value }
+    Select-Xml -XPath "//Directory/@key" |
+    ForEach-Object { $_.Node.Value }
 
     Write-Host "IDs: $section_ids"
-    Write-Host "Path: $directory\$folder"
-    $encodedPath = UrlEncode("$directory\$folder")
+    Write-Host "Path: $ $mount/$path"
+    $encodedPath = UrlEncode("$mount/$path")
 
-    try {
+    if (Test-Path $mount/$path) {
+        Write-Host "Path exists"
         # Trigger the library update for the specific folder
         foreach ($section_id in $section_ids) {
-            $final_url = "${plexUrl}/library/sections/${section_id}/refresh?path=${encodedPath}&X-Plex-Token=${plexToken}"
-            
-            Write-Host "Encoded argument: $encodedPath"
-            Write-Host "Section ID: $section_id"
-            Write-Host "Final URL: $final_url"
+        $final_url = "${plexUrl}/library/sections/${section_id}/refresh?path=${encodedPath}&X-Plex-Token=${plexToken}"
 
-            Invoke-WebRequest -Uri $final_url -UseBasicParsing -Method Get
+        Write-Host "Encoded argument: $encodedPath"
+        Write-Host "Section ID: $section_id"
+        Write-Host "Final URL: $final_url"
 
-            Write-Host "Partial refresh request successful for: $($folder.FullName)"
+        $request = Invoke-WebRequest -Uri $final_url -UseBasicParsing -Method Get
+
+        Write-Host $request
+
+        Write-Host "Partial refresh request successful for: $($path)"
         }
-    } catch {
-        Write-Host "Error refreshing: $($folder.FullName)"
-        Write-Host "Error details: $_"
-    }
-}
-
-# Function to trigger library updates for all folders modified within the last 5 minutes
-function UpdateFoldersWithinLast5Minutes($directories) {
-    $startTime = (Get-Date).AddMinutes(-5)
-
-    Start-Sleep -Seconds 5
-
-    foreach ($directory in $directories) {
-        $folders = Get-ChildItem -Path $directory -Directory | Where-Object { $_.LastWriteTime -gt $startTime }
-
-        if ($folders.Count -gt 0) {
-            Write-Host "Folders found in $directory modified within the last 5 minutes:"
-            
-            # Introduce a 10-second delay before triggering the library update for each folder
-            Start-Sleep -Seconds 10
-
-            foreach ($folder in $folders) {
-                UpdateFolder $folder $directory
-            }
-        } else {
-            Write-Host "No folders found in $directory modified within the last 5 minutes."
+    } else {
+        if (!$retries -eq 0) {
+            $retries--
+            Write-Host "Retries: $retries"
+            Write-Host "Path not found. Trying again..."
+            Start-Sleep -Seconds 1
+            UpdateFolder $retries
+        }
+        else {
+            Write-Host "The path does not exist."
         }
     }
 }
 
-# Example usage - update folders modified within the last 5 minutes - EDIT BELOW
-$directoriesToUpdate = @("T:\movies","T:\anime","T:\shows")
-UpdateFoldersWithinLast5Minutes $directoriesToUpdate
-
-Write-Host "All updated sections partially refreshed."
+UpdateFolder $retryAmount
