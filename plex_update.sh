989f70d2f1d71d8815fe9e83e758b9560f6c832f
Ben Adrian Sarmiento <me@bensarmiento.com>
2024-03-31 19:00:31 +0200

    Update plex_update.sh

--- plex_update.sh
+++ plex_update.sh
@@ -7,8 +7,8 @@
 # docker compose exec zurg apk add libxml2-utils
 # sudo apt install libxml2-utils
 
-plex_url="http://yourplexip:32400" # If you're using zurg inside a Docker container, by default it is 172.17.0.1:32400
-token="yourplextoken" # open Plex in a browser, open dev console and copy-paste this: window.localStorage.getItem("myPlexAccessToken")
+plex_url="http://<url>" # If you're using zurg inside a Docker container, by default it is 172.17.0.1:32400
+token="<token>" # open Plex in a browser, open dev console and copy-paste this: window.localStorage.getItem("myPlexAccessToken")
 zurg_mount="/mnt/zurg" # replace with your zurg mount path, ensure this is what Plex sees
 
 # Get the list of section IDs
@@ -16,29 +16,20 @@ section_ids=$(curl -sLX GET "$plex_url/library/sections" -H "X-Plex-Token: $toke
 
 for arg in "$@"
 do
-    modified_arg="$zurg_mount/$arg"
+    parsed_arg="${arg//\\}"
+    echo $parsed_arg
+    modified_arg="$zurg_mount/$parsed_arg"
     echo "Detected update on: $arg"
     echo "Absolute path: $modified_arg"
 
-    encoded_arg=$(echo -n "$modified_arg" | python3 -c "import sys, urllib.parse as ul; print (ul.quote_plus(sys.stdin.read()))")
-
-    if [ -z "$encoded_arg" ]; then
-        echo "Error: Encoded argument is empty. Check the input or encoding process."
-        continue
-    fi
-
     for section_id in $section_ids
     do
-        final_url="${plex_url}/library/sections/${section_id}/refresh?path=${encoded_arg}&X-Plex-Token=${token}"
-
-        echo "Encoded argument: $encoded_arg"
         echo "Section ID: $section_id"
-        echo "Final URL: $final_url"
 
-        curl -s "$final_url"
+        curl -G -H "X-Plex-Token: $token" --data-urlencode "path=$modified_arg" $plex_url/library/sections/$section_id/refresh
     done
 done
 
 echo "All updated sections refreshed"
 
-# credits to godver3
+# credits to godver3, wasabipls
